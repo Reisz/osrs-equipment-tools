007e359945967fbc06a5336fdcb9a34f7106d150
Daniel <Reisz@users.noreply.github.com>
2020-11-26 17:18:42 +0100

    Fix formatting

--- data-processor/src/filter/heraldic.rs
+++ data-processor/src/filter/heraldic.rs
@@ -25,8 +25,16 @@ const CRESTS: &[&str] = &[
 /// - [Heraldic helmet](https://oldschool.runescape.wiki/w/Heraldic_helmet_(Construction))
 /// - [Heraldic kiteshield](https://oldschool.runescape.wiki/w/Heraldic_kiteshield_(Construction))
 pub fn add_wiki_names(set: &mut HashSet<String>) {
-    set.extend(CRESTS.iter().map(|c| format!("Steel heraldic helm ({})", c)));
-    set.extend(CRESTS.iter().map(|c| format!("Adamant heraldic helm ({})", c)));
+    set.extend(
+        CRESTS
+            .iter()
+            .map(|c| format!("Steel heraldic helm ({})", c)),
+    );
+    set.extend(
+        CRESTS
+            .iter()
+            .map(|c| format!("Adamant heraldic helm ({})", c)),
+    );
     set.extend(CRESTS.iter().map(|c| format!("Rune heraldic helm ({})", c)));
 
     set.extend(CRESTS.iter().map(|c| format!("Steel kiteshield ({})", c)));
