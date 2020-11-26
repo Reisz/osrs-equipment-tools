a7cc8cb608dcb4f0b8b56945ec6b5f8bbb493490
Daniel <Reisz@users.noreply.github.com>
2020-11-26 11:54:27 +0100

    Add trailblazer expressions for Morytania items

--- trailblazer/src/data/morytania.rs
+++ trailblazer/src/data/morytania.rs
@@ -1,7 +1,10 @@
 //! Items mainly locked by Morytania.
 
 use super::ExprMap;
-use crate::{bool_expr::BoolExpr, vars::Region};
+use crate::{
+    bool_expr::{BoolExpr, BoolExprBuilder},
+    vars::Region,
+};
 
 fn expr() -> BoolExpr<Region> {
     BoolExpr::new(Region::Morytania)
@@ -9,10 +12,22 @@ fn expr() -> BoolExpr<Region> {
 
 /// Add requirements for the following items:
 /// - [Barrows equipment](https://oldschool.runescape.wiki/w/Barrows_equipment)
+/// - [Grotesque Guardians](https://oldschool.runescape.wiki/w/Grotesque_Guardians) / Gargoyles
+///     - The super-rare [Bandos boots](https://oldschool.runescape.wiki/w/Bandos_boots) drop
+///       from elite clue steps is not counted here.
+/// - [Theatre of Blood](https://oldschool.runescape.wiki/w/Theatre_of_Blood#Loot_table)
+/// - [The Nightmare](https://oldschool.runescape.wiki/w/The_Nightmare)
+/// - Other
+///     - [Amulet of blood fury](https://oldschool.runescape.wiki/w/Amulet_of_blood_fury)
+///     - [Black mask](https://oldschool.runescape.wiki/w/Black_mask)
 pub fn add_morytania(map: &mut ExprMap) {
     add_barrows(map);
+    add_guardians(map);
+    add_tob(map);
+    add_nightmare(map);
+    add_other(map);
 }
-/// Add Morytania requirement to the barrows pieces.
+
 fn add_barrows(map: &mut ExprMap) {
     map.insert("Ahrim's hood".to_string(), expr());
     map.insert("Ahrim's robeskirt".to_string(), expr());
@@ -43,4 +58,51 @@ fn add_barrows(map: &mut ExprMap) {
     map.insert("Verac's flail".to_string(), expr());
     map.insert("Verac's helm".to_string(), expr());
     map.insert("Verac's plateskirt".to_string(), expr());
+}
+
+fn add_guardians(map: &mut ExprMap) {
+    map.insert("Granite maul".to_string(), expr());
+    map.insert("Granite gloves".to_string(), expr());
+    map.insert("Granite ring".to_string(), expr());
+    map.insert("Granite hammer".to_string(), expr());
+
+    let mut expr = BoolExprBuilder::new();
+    expr.var(Region::Morytania);
+    expr.var(Region::Asgarnia);
+    expr.and();
+    let expr = expr.finalize().unwrap();
+    map.insert("Guardian boots".to_string(), expr);
+}
+
+fn add_tob(map: &mut ExprMap) {
+    map.insert("Ghrazi rapier".to_string(), expr());
+    map.insert("Sanguinesti staff".to_string(), expr());
+    map.insert("Scythe of vitur".to_string(), expr());
+
+    map.insert("Justiciar faceguard".to_string(), expr());
+    map.insert("Justiciar chestguard".to_string(), expr());
+    map.insert("Justiciar legguards".to_string(), expr());
+
+    let mut expr = BoolExprBuilder::new();
+    expr.var(Region::Morytania);
+    expr.var(Region::Asgarnia);
+    expr.and();
+    let expr = expr.finalize().unwrap();
+    map.insert("Avernic defender".to_string(), expr);
+}
+
+fn add_nightmare(map: &mut ExprMap) {
+    map.insert("Nightmare staff".to_string(), expr());
+
+    map.insert("Inquisitor's great helm".to_string(), expr());
+    map.insert("Inquisitor's hauberk".to_string(), expr());
+    map.insert("Inquisitor's plateskirt".to_string(), expr());
+    map.insert("Inquisitor's mace".to_string(), expr());
+}
+
+fn add_other(map: &mut ExprMap) {
+    map.insert("Black mask".to_string(), expr());
+    map.insert("Slayer helmet".to_string(), expr());
+    map.insert("Slayer helmet (i)".to_string(), expr());
+    map.insert("Amulet of blood fury".to_string(), expr());
 }
