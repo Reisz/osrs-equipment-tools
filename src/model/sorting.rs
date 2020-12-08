b3b89e7c446deccbc7ab4feda6cefc1c49d951cf
Daniel <Reisz@users.noreply.github.com>
2020-12-08 15:12:48 +0100

    Add sorting preset

--- src/model/sorting.rs
+++ src/model/sorting.rs
@@ -3,6 +3,7 @@
 use std::cmp::Ordering;
 
 use data::Item;
+use enum_iterator::IntoEnumIterator;
 use seed::prelude::{LocalStorage, Orders, WebStorage};
 use serde::{Deserialize, Serialize};
 
@@ -13,6 +14,14 @@ const STORAGE_KEY: &str = "sorting";
 /// Fragments for building a sorting method.
 #[derive(Debug, Clone, Copy, Deserialize, Serialize)]
 pub enum SortingFragment {
+    /// Average of melee attack bonuses
+    MeleeAttackAvg,
+    /// Magic attack bonus
+    MagicAttack,
+    /// Ranged attack bonus
+    RangedAttack,
+    /// Median of all defensive bonuses
+    DefenceMedian,
     /// Sort by melee strength
     MeleeStrength,
     /// Sort by ranged strength
@@ -24,21 +33,90 @@ pub enum SortingFragment {
 }
 
 impl SortingFragment {
-    fn ordering(&self, a: &Item, b: &Item) -> Ordering {
+    fn get(&self, i: &Item) -> i16 {
         match self {
-            Self::MeleeStrength => a
-                .stats
-                .melee_strength
-                .cmp(&b.stats.melee_strength)
-                .reverse(),
-            Self::RangedStrength => a
-                .stats
-                .ranged_strength
-                .cmp(&b.stats.ranged_strength)
-                .reverse(),
-            Self::MagicDamage => a.stats.magic_damage.cmp(&b.stats.magic_damage).reverse(),
-            Self::Prayer => a.stats.prayer.cmp(&b.stats.prayer).reverse(),
+            Self::MeleeAttackAvg => {
+                let a = &i.stats.attack;
+                (a.stab + a.slash + a.crush) / 3
+            }
+            Self::MagicAttack => i.stats.attack.magic,
+            Self::RangedAttack => i.stats.attack.ranged,
+            Self::DefenceMedian => {
+                let d = &i.stats.defence;
+                let mut stats = [d.stab, d.slash, d.crush, d.magic, d.ranged];
+                stats.sort_unstable();
+                stats[2]
+            }
+            Self::MeleeStrength => i.stats.melee_strength,
+            Self::RangedStrength => i.stats.melee_strength,
+            Self::MagicDamage => i.stats.magic_damage.into(),
+            Self::Prayer => i.stats.prayer,
         }
+    }
+
+    fn is_descending(&self) -> bool {
+        true
+    }
+
+    fn ordering(&self, a: &Item, b: &Item) -> Ordering {
+        let ord = self.get(a).cmp(&self.get(b));
+
+        if self.is_descending() {
+            ord.reverse()
+        } else {
+            ord
+        }
+    }
+}
+
+/// Presets for sorting
+#[derive(Debug, IntoEnumIterator)]
+pub enum SortingPreset {
+    /// Prioritize strength bonus -> attack avg -> prayer -> defence median
+    Melee,
+    /// Prioritize damage bonus -> attack -> prayer -> defence median
+    Magic,
+    /// Prioritize strength bonus -> attack -> prayer -> defence median
+    Ranged,
+    /// Prioritize prayer -> defence median
+    Prayer,
+}
+
+const MELEE_PRESET: &[SortingFragment] = &[
+    SortingFragment::MeleeStrength,
+    SortingFragment::MeleeAttackAvg,
+    SortingFragment::Prayer,
+    SortingFragment::DefenceMedian,
+];
+
+const MAGIC_PRESET: &[SortingFragment] = &[
+    SortingFragment::MagicDamage,
+    SortingFragment::MagicAttack,
+    SortingFragment::Prayer,
+    SortingFragment::DefenceMedian,
+];
+
+const RANGED_PRESET: &[SortingFragment] = &[
+    SortingFragment::RangedStrength,
+    SortingFragment::RangedAttack,
+    SortingFragment::Prayer,
+    SortingFragment::DefenceMedian,
+];
+
+const PRAYER_PRESET: &[SortingFragment] =
+    &[SortingFragment::Prayer, SortingFragment::DefenceMedian];
+
+impl SortingPreset {
+    /// Apply this preset to `sorting`.
+    pub fn apply_to(&self, sorting: &mut Sorting) {
+        sorting.0.clear();
+        let list = match self {
+            Self::Melee => MELEE_PRESET,
+            Self::Magic => MAGIC_PRESET,
+            Self::Ranged => RANGED_PRESET,
+            Self::Prayer => PRAYER_PRESET,
+        };
+        sorting.0.extend_from_slice(list);
     }
 }
 
@@ -75,9 +153,14 @@ impl Sorting {
 }
 
 /// Messages to manipulate sorting order
-pub enum SortingMsg {}
+pub enum SortingMsg {
+    /// Apply a preset to the sorting order.
+    ApplyPreset(SortingPreset),
+}
 
 /// Change sorting based on [`SortingMsg`].
-pub fn update(msg: SortingMsg, _sorting: &mut Sorting, _orders: &mut impl Orders<Msg>) {
-    match msg {}
+pub fn update(msg: SortingMsg, sorting: &mut Sorting, _orders: &mut impl Orders<Msg>) {
+    match msg {
+        SortingMsg::ApplyPreset(preset) => preset.apply_to(sorting),
+    }
 }
