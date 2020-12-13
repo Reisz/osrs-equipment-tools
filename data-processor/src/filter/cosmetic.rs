a8ebb26a13e88a1d36d81f62cd945a271f600acd
Daniel <Reisz@users.noreply.github.com>
2020-12-13 13:10:42 +0100

    Filter out Blacksmith's helm

--- data-processor/src/filter/cosmetic.rs
+++ data-processor/src/filter/cosmetic.rs
@@ -23,6 +23,7 @@ use std::collections::HashSet;
 /// - [Ornate armour](https://oldschool.runescape.wiki/w/Ornate_armour)
 /// - [Amulet of glory (t)](https://oldschool.runescape.wiki/w/Amulet_of_glory_(t)#Uncharged)
 /// - [Spiked boots](https://oldschool.runescape.wiki/w/Spiked_boots )
+/// - [Blacksmith's helm](https://oldschool.runescape.wiki/w/Blacksmith's_helm)
 pub fn add_names(set: &mut HashSet<String>) {
     set.insert("Twisted ancestral hat".to_string());
     set.insert("Twisted ancestral robe bottom".to_string());
@@ -130,6 +131,8 @@ pub fn add_names(set: &mut HashSet<String>) {
     set.extend((1..=6).map(|i| format!("Amulet of glory (t{})", i)));
 
     set.insert("Spiked boots".to_string());
+
+    set.insert("Blacksmith's helm".to_string());
 }
 
 /// Filters out the following cosmetic variants:
